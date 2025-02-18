99894e23db06dd4a64df7ce6e9bfd0b6ca6bfc56
fehoon.Hyeon <clearch21@gmail.com>
2025-02-19 00:04:03 +0900

    환경설정, 경로조정

--- src/main/java/com/bc/msa/order/config/SwaggerConfig.java
+++ src/main/java/com/bc/msa/order/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.bc.msa.order.common.config;
+package com.bc.msa.order.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
--- src/main/java/com/bc/msa/order/controller/HomeController.java
+++ src/main/java/com/bc/msa/order/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.bc.msa.order;
+package com.bc.msa.order.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
